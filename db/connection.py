d46a0cd2f673c34b4f69a6f064f8daff4dc86399
Yordy2001 <kingyordi2001@gmail.com>
2022-11-18 15:27:03 -0500

    add db connection

--- db/connection.py
+++ db/connection.py
@@ -1,5 +1,27 @@
+import os
+from dotenv import load_dotenv
 import psycopg2
 
-conexion = psycopg2.connect(host="localhost",database="list_system", user="postgres", password="20012020")
-cur = conexion.cursor()
+load_dotenv()
 
+
+class Database:
+    cursor = None
+    conn = None
+
+    def connect(self):
+        try:
+            conn = psycopg2.connect(
+                host=str("{}".format(os.getenv('DBHOST'))),
+                dbname=str("{}".format(os.getenv('DBNAME'))),
+                user=str("{}".format(os.getenv('DBUSER'))),
+                password=str("{}".format(os.getenv('DBPASSWORD'))),
+                port=os.getenv('DBPORT'))
+            self.cursor = conn.cursor()
+        except Exception as e:
+            print(e)
+
+
+database = Database()
+database.connect()
+# database.close()
